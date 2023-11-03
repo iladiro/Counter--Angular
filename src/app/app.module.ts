08450f5f8be144470e37c1d991b06f80c507d3a0
iladiro <dirosa.ilaria@gmail.com>
2023-11-03 11:06:22 +0100

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { IladiroCounterModule } from 'Counter';
+import { IladiroCounterModule } from 'projects/counter/src/public-api';
 
 @NgModule({
   declarations: [
